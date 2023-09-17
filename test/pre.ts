f7cd3a7cb68aff033e88625350b5b5b18b020177
Jacob Hummer <jcbhmr@outlook.com>
2023-09-17 17:45:31 -0500

    simpler if

--- test/pre.ts
+++ test/pre.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env -S deno run -Aq
 import * as core from "@actions/core";
-
 const message = core.getInput("message", { required: true });
 console.log(message);
+core.setOutput("message", message);
