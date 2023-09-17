773e1aae5832052a7113364c10861a24a60a476e
Jacob Hummer <jcbhmr@outlook.com>
2023-09-17 16:19:46 -0500

    dashed instead

--- src/main.js
+++ src/main.js
@@ -4,5 +4,11 @@ import { join } from "node:path";
 
 process.env.DENO_INSTALL = join(process.env.RUNNER_TEMP, "using-deno1");
 await $`curl -fsSL https://deno.land/x/install/install.sh | sh`;
-process.env.PATH = `${process.env.PATH}:${join(process.env.DENO_INSTALL, "bin")}`;
-await $`deno run -Aq ${join(process.env.INPUT_GITHUB_ACTION_PATH, process.env.INPUT_MAIN)}`;
+process.env.PATH = `${process.env.PATH}:${join(
+  process.env.DENO_INSTALL,
+  "bin"
+)}`;
+await $`deno run -Aq ${join(
+  process.env["INPUT_GITHUB-ACTION-PATH"],
+  process.env.INPUT_MAIN
+)}`;
