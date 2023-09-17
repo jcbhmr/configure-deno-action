2ae25f1dedbaed5b046e4e441a74a86435c013ec
Jacob Hummer <jcbhmr@outlook.com>
2023-09-17 16:16:27 -0500

    string instead of var

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ import { join } from "node:path";
 
 process.env.DENO_INSTALL = join(process.env.RUNNER_TEMP, "using-deno1");
 await $`curl -fsSL https://deno.land/x/install/install.sh | sh`;
-process.env.PATH = `${process.env.PATH}:${join(process.env.DENO_INSTALL, bin)}`;
+process.env.PATH = `${process.env.PATH}:${join(process.env.DENO_INSTALL, "bin")}`;
 await $`deno run -Aq ${join(process.env.INPUT_GITHUB_ACTION_PATH, process.env.INPUT_MAIN)}`;
