5c79edd3b7a5f2994b08bdcf4b2b9d9e8800057b
Jacob Hummer <jcbhmr@outlook.com>
2023-09-18 16:41:25 -0500

    clearer code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { createRequire } from "node:module";
-import { basename, dirname, resolve } from "node:path";
+import { basename, dirname, join, resolve } from "node:path";
 import { findUp } from "find-up";
 import { openAsBlob } from "node:fs";
 import * as YAML from "yaml";
@@ -8,7 +8,8 @@ const runtimes = {
   deno1: () => import("./deno1.js"),
   bun1: () => import("./bun1.js"),
 };
-const entry = createRequire("/").resolve(process.argv[1]);
+const require = createRequire("/");
+const entry = require.resolve(process.argv[1]);
 const action = await (async () => {
   const path = await findUp(["action.yml", "action.yaml"], {
     cwd: dirname(entry),
@@ -19,8 +20,9 @@ const action = await (async () => {
   return file;
 })();
 const { runs, ".runs": $runs } = YAML.parse(await action.text());
-const stage = ["pre", "main", "post"]
-  .filter((x) => $runs[x])
-  .find((x) => resolve(action.webkitRelativePath, runs[x]) === entry);
+const stage = ["pre", "main", "post"].find((x) => {
+  const absolute = join(action.webkitRelativePath, runs[x] ?? "");
+  return require.resolve(absolute) === entry;
+});
 const { default: runtime } = await runtimes[$runs.using]();
 await runtime(action, $runs[stage]);
