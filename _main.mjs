60a66c66714be7b5a33fdd6266935ee919d653a2
Jacob Hummer <jcbhmr@outlook.com>
2023-11-08 23:58:53 -0600

    try this

--- _main.mjs
+++ _main.mjs
@@ -5,16 +5,16 @@ import { createGunzip } from "node:zlib";
 import { chmod } from "node:fs/promises";
 import { once } from "node:events";
 const name = "main"; // 👈 CHANGE ME!
-const target = {
-  "win32,x64": "x86_64-pc-windows-msvc",
-  "darwin,x64": "x86_64-apple-darwin",
-  "linux,x64": "x86_64-unknown-linux-gnu",
-}[[process.platform, process.arch].toString()];
 const ext = process.platform === "win32" ? ".exe" : "";
 let file;
 if (dirname(process.argv[1]).startsWith(process.cwd())) {
   file = join(dirname(process.argv[1]), name + ext);
 } else {
+  const target = {
+    "win32,x64": "x86_64-pc-windows-msvc",
+    "darwin,x64": "x86_64-apple-darwin",
+    "linux,x64": "x86_64-unknown-linux-gnu",
+  }[[process.platform, process.arch].toString()];
   file = join(dirname(process.argv[1]), `${name}-${target}${ext}`);
   await pipeline(
     createReadStream(`${file}.gz`),
