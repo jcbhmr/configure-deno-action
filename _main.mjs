62f17b1c580602fcd3c7a3625a053e7a4e96e876
Jacob Hummer <jcbhmr@outlook.com>
2023-11-08 23:51:10 -0600

    try this

--- _main.mjs
+++ _main.mjs
@@ -1,29 +1,27 @@
+import { createReadStream, createWriteStream } from "node:fs";
 import { spawn } from "node:child_process";
+import { dirname, join } from "node:path";
+import { createGunzip } from "node:zlib";
+import { chmod } from "node:fs/promises";
 import { once } from "node:events";
-import { join, dirname } from "node:path";
-import { existsSync } from "node:fs";
-const file = join(dirname(process.argv[1]), "main.ts"); // 👈 CHANGE ME!
-const response = await fetch("https://deno.com/versions.json");
-const json = await response.json();
-const tag = json.cli.find((x) => x.startsWith("v1."));
-const version = tag.slice(1);
-const DENO_INSTALL = join(
-  process.env.RUNNER_TOOL_CACHE,
-  "deno",
-  version,
-  process.arch,
-);
-if (!existsSync(DENO_INSTALL)) {
-  const subprocess1 = spawn(
-    `curl -fsSL https://deno.land/x/install/install.sh | sh -s "$tag"`,
-    { shell: "bash", env: { ...process.env, DENO_INSTALL, tag } },
+const name = "main"; // 👈 CHANGE ME!
+const target = {
+  "win32,x64": "x86_64-pc-windows-msvc",
+  "darwin,x64": "x86_64-apple-darwin",
+  "linux,x64": "x86_64-unknown-linux-gnu",
+}[[process.platform, process.arch].toString()];
+const ext = process.platform === "win32" ? ".exe" : "";
+let file;
+if (dirname(process.argv[1]).startsWith(process.cwd())) {
+  file = join(dirname(process.argv[1]), name + ext);
+} else {
+  file = join(dirname(process.argv[1]), `${name}-${target}${ext}`);
+  await pipeline(
+    createReadStream(`${file}.gz`),
+    createGunzip(),
+    createWriteStream(file)
   );
-  await once(subprocess1, "exit");
+  await chmod(file, 0o755);
 }
-const subprocess2 = spawn(
-  join(DENO_INSTALL, "bin", "deno"),
-  ["run", "-Aq", file],
-  { stdio: "inherit" },
-);
-await once(subprocess2, "spawn");
-subprocess2.on("exit", (x) => process.exit(x));
+const subprocess = spawn(file, { stdio: "inherit" });
+process.exitCode = (await once(subprocess, "exit"))[0];
